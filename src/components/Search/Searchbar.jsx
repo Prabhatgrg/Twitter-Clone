08f1fcb8b1054929f179cad86250c5c7d3b0b1e2
Neer Shrestha <shrestha.neer@hotmail.com>
2023-09-20 12:41:04 +0545

    search bar

--- src/components/Search/Searchbar.jsx
+++ src/components/Search/Searchbar.jsx
@@ -6,13 +6,10 @@ const Searchbar = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <FiSearch />
-                <input type="text" className="p-2 border-0" />
-                <div className="p-3 boder-2"></div>
-            </form>
-        </div>
+        <form className="flex gap-2 items-center bg-neutral-800 px-5 py-3 rounded-full" onSubmit={(e) => handleSubmit(e)}>
+            <FiSearch />
+            <input type="text" className="p-2 border-0 bg-transparent focus:outline-0 grow" />
+        </form>
     );
 };
 
