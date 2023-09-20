d70c81bead9259778c1bad7a8eb19f15cc23138b
Neer Shrestha <shrestha.neer@hotmail.com>
2023-09-20 12:04:42 +0545

    tailwind and react icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import twitterLogo from "./assets/img/logo.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Searchbar from "./componets/Searchbar";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
+            <Searchbar />
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
