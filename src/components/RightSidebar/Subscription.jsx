7d300f9583e9da670a0df94e39b91191c96b80ec
Neer Shrestha <shrestha.neer@hotmail.com>
2023-09-21 20:14:21 +0545

    subscription

--- src/components/RightSidebar/Subscription.jsx
+++ src/components/RightSidebar/Subscription.jsx
@@ -4,7 +4,7 @@ const Subscription = () => {
             <div className="bg-[#16181C] py-3 px-4 rounded-[1rem]">
                 <h2 className="text-xl font-bold mb-2">Subscribe to Premium</h2>
                 <p className="font-semibold mb-3">Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
-                <a href="#" className="inline-block py-2 leading-4 px-4 rounded-full bg-[var(--clr-primary)] hover:bg-[var(--clr-primary-hover)] text-white hover:text-white">
+                <a href="#" className="inline-block py-3 leading-4 px-4 rounded-full bg-[var(--clr-primary)] hover:bg-[var(--clr-primary-hover)] text-white hover:text-white">
                     Subscribe
                 </a>
             </div>
